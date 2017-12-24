9f48edca01a164dc6e479c8ee6cdbbbeb435dbdf
SciFi1818 <31384079+SciFi1818@users.noreply.github.com>
2017-12-24 13:26:04 +0530

    Update Application.mk

--- lib/src/main/jni/Application.mk
+++ lib/src/main/jni/Application.mk
@@ -1,4 +1,10 @@
 APP_ABI := armeabi armeabi-v7a x86
 APP_PLATFORM := android-14
-APP_STL := stlport_static
+#APP_STL := stlport_static
 APP_OPTIM := release
+NDK_TOOLCHAIN_VERSION := 4.8
+# APP_STL := stlport_shared  --> does not seem to contain C++11 features
+APP_STL := gnustl_shared
+
+# Enable c++11 extentions in source code
+APP_CPPFLAGS += -std=c++11
